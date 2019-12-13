c0ac17945256f456e5442917f88d9aacb2bad053
Vaduha-git <47142165+Vaduha-git@users.noreply.github.com>
2019-12-13 22:15:43 +0700

    Updates

---
+++ precache-manifest.046baccfd8f7c23eac3bf766345aa038.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63471a22dc7617814a6735613b766856",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e9add3698f7cded2b3e",
+    "url": "./static/css/main.d87bc567.chunk.css"
+  },
+  {
+    "revision": "3116533a53b74b1c31b1",
+    "url": "./static/js/2.220182bf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.220182bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e9add3698f7cded2b3e",
+    "url": "./static/js/main.1d57f0ad.chunk.js"
+  },
+  {
+    "revision": "353824c915518b56d5bd",
+    "url": "./static/js/runtime-main.1130748e.js"
+  }
+]);
